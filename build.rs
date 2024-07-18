a69389ce54802793678c90973939759fac0d5e3b
Mohammed Alyousef <may642_2000@hotmail.com>
2024-07-18 22:36:34 +0300

    update C++ standard for msvc

--- build.rs
+++ build.rs
@@ -7,6 +7,7 @@ fn main() {
         .file("src/cfl_flow.cpp")
         .cpp(true)
         .flag_if_supported("-std=c++17")
+        .flag_if_supported("/std:c++17")
         .flag_if_supported("-w")
         .include(&fltk_out_dir.join("include"))
         .compile("fltk_flow");
